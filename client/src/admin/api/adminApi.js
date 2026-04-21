f432f8fa6d6ca3cb80b705d10fcc1f85a2f256e3
Rajesh Kumar Sarkar <rajesh36.sarkar@gmail.com>
2026-04-21 15:47:37 +0530

    change with api

--- client/src/admin/api/adminApi.js
+++ client/src/admin/api/adminApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend-name.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // 🔐 Attach token
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://nswpl-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getProducts = () => API.get("/products");
