9380fdad48044bbce9ac1bd1cacd9ebce261069a
Rajesh Kumar Sarkar <rajesh36.sarkar@gmail.com>
2026-04-21 18:11:30 +0530

    changes on api

--- src/services/api.js
+++ src/services/api.js
@@ -4,5 +4,6 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-export const getProducts = () => API.get("/products");
-export const createOrder = (data) => API.post("/orders", data);
+
+export const getProducts = () => API.get("/api/products");
+export const createOrder = (data) => API.post("/api/orders", data);
